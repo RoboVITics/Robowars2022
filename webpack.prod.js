d0c5dce25755794fdb7217e3f243dca0d3646057
Henit Chobisa <chobisa.henit@gmail.com>
2022-09-23 15:49:04 +0530

    feat: changed output to public

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
     mode : "production",
     output : {
         filename : "bundle.[contenthash].js",
-        path : path.resolve(__dirname, "dist")
+        path : path.resolve(__dirname, "public")
     },
     plugins : [
         new CleanWebpackPlugin(),
