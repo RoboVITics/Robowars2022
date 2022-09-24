df8f943c92875c22c95c0670db3c3f9ee9fb0bc0
Henit Chobisa <chobisa.henit@gmail.com>
2022-09-24 11:15:50 +0530

    fix : optimized with background layer

--- webpack.common.js
+++ webpack.common.js
@@ -13,4 +13,31 @@ module.exports = {
       },
     }),
   ],
+  module : {
+    rules : [
+        {
+           test: /\.(html)$/,
+           use: ["html-loader"]
+        },
+       //  {
+       //     test: /\.(svg|png|jp(e*)g)$/,
+       //     use: {
+       //         loader: "url-loader",
+       //         type: "asset/resource",
+       //         options : {
+       //             esModule:false,
+       //             name: "imgs/[name]-[hash].[ext]",
+       //             limit: 80000
+       //         }
+       //     }
+       //  }
+       {
+           test: /\.(svg|png|jp(e*)g)$/,
+           type: "asset/resource",
+           generator: {
+               filename: 'imgs/[name].[hash][ext]'
+           } 
+       }
+   ]
+  }
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -18,6 +18,7 @@ module.exports = merge(common, {
     ],
     module : { 
         rules : [
+            
             {
                 test : /\.css$/,
                 use : [MiniCssExtractPlugin.loader,"css-loader"]
